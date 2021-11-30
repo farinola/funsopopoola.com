13bfa45204cac19bfe82e87b0eae41d38c864639
farinola <fp.arinola@gmail.com>
2021-11-30 01:14:33 +0000

    chore: merged with main

---
+++ components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="bg-white p-4 md:flex md:justify-between">
+    <div class="flex justify-between border-b-2 md:border-0 items-center">
+        <p class="font-bold">
+            <span class="text-purple-700">funso</span><span>popoola</span>
+        </p>
+        <p class="text-gray-700 w-4 md:hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mb-0" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+            </svg>
+        </p>
+    </div>
+    <div class="flex flex-col md:flex-row md:justify-between md:max-w-md md:w-1/4">
+        <p>Experiments</p>
+        <p>Jobs</p>
+    </div>
+    
+</div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="bg-gray-100 min-h-screen">
+    <Header />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
